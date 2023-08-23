01b3a834eda50b9346f20d95b844994fb9a260b8
0xsamuel-eth <sammmastenn@gmail.com>
2023-08-23 16:29:04 -0400

    Imported libraries to run PCA

---
+++ pca.py
@@ -0,0 +1,12 @@
+import numpy as np
+from sklearn.decomposition import PCA
+
+X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+pca = PCA(n_components=2)
+
+pca.fit(X)
+#output
+print(pca.explained_variance_ratio_)
+#output
+print(pca.singular_values_)
+#output
